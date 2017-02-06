f6e74676affdbc7470c7bee21f4097d1d953d064
AlekseySorokin <aleksey.sorokin1106@yandex.ru>
2017-02-06 13:59:21 +0400

    Building project and fix bower main files.

--- dist/components.controller.js
+++ dist/components.controller.js
@@ -19,15 +19,60 @@
                             component: {
                                 name: 'ue-tinymce',
                                 settings: {
-                                    "name": "html_text",
-                                    "label": "Форматированный текст",
-                                    "hint": "Это большое и красивое текстовое поле",
-                                    "required": false,
-                                    "expandable": true,
-                                    "readonly": false,
-                                    "multiname": "new_value",
-                                    "multiple": true,
-                                    width: 12
+                                    label: 'ue-tinymce Одиночный режим',
+                                    hint: 'Это большое и красивое текстовое поле',
+                                    required: false,
+                                    readonly: false,
+                                    multiname: 'new_value',
+                                    multiple: false,
+                                    width: 8,
+                                    defaultValue: '<p>Текст</p>'
+                                }
+                            }
+                        },
+                        {
+                            component: {
+                                name: 'ue-tinymce',
+                                settings: {
+                                    label: 'ue-tinymce Множественный режим',
+                                    hint: 'Это большое и красивое текстовое поле',
+                                    required: false,
+                                    readonly: false,
+                                    multiname: 'new_value',
+                                    multiple: true,
+                                    width: 12,
+                                    defaultValue: ['Текст']
+                                }
+                            }
+                        },
+                        {
+                            component: {
+                                name: 'ue-tinymce',
+                                settings: {
+                                    label: 'ue-tinymce Только для чтения',
+                                    hint: 'Это большое и красивое текстовое поле',
+                                    required: false,
+                                    readonly: true,
+                                    multiname: 'new_value',
+                                    multiple: true,
+                                    width: 12,
+                                    defaultValue: ['<p>Текст</p>']
+                                }
+                            }
+                        },
+                        {
+                            component: {
+                                name: 'ue-tinymce',
+                                settings: {
+                                    label: 'ue-tinymce Неактивное (disabled)',
+                                    hint: 'Это большое и красивое текстовое поле',
+                                    required: false,
+                                    readonly: true,
+                                    disabled: true,
+                                    multiname: 'new_value',
+                                    multiple: true,
+                                    width: 12,
+                                    defaultValue: ['Текст 1', 'Текст 2']
                                 }
                             }
                         }
