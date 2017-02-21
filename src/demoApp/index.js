999bfaa675f4dc6ca21b87e33c291c1a0398a80a
AlekseySorokin <aleksey.sorokin1106@yandex.ru>
2017-02-21 17:12:13 +0400

    Update dependences.

--- src/demoApp/index.js
+++ src/demoApp/index.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    angular.module('demoApp', ['ui.router','universal.editor', 'ue-tinymce']);
+    angular.module('demoApp', ['ui.router','universal-editor', 'ue-tinymce']);
 
 
     angular
